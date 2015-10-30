9c9c53dcfbd67c1af8cad3089e9aeb4bc94a9005
rubenmoya <rmoyarodriguez@gmail.com>
2015-10-30 11:24:34 +0100

    Bishop moves.

--- chess_validator.rb
+++ chess_validator.rb
@@ -1,13 +1,4 @@
-# [
-#   [:bR, :bN, :bB, :bQ, :bK, :bB, :bN, :bR],
-#   [:bP, :bP, :bP, :bP, :bP, :bP, :bP, :bP],
-#   [nil, nil, nil, nil, nil, nil, nil, nil],
-#   [nil, nil, nil, nil, nil, nil, nil, nil],
-#   [nil, nil, nil, nil, nil, nil, nil, nil],
-#   [nil, nil, nil, nil, nil, nil, nil, nil],
-#   [:wP, :wP, :wP, :wP, :wP, :wP, :wP, :wP],
-#   [:aR, :aN, :aB, :aQ, :aK, :aB, :aN, :aR]
-# ]
+require "pry"
 
 board = [
   [:bR, nil, nil, nil, nil, nil, nil, :bR],
@@ -20,30 +11,62 @@ board = [
   [:bR, nil, nil, nil, nil, nil, nil, :wR]
 ]
 
+module CheckMove
+  def rook_move
+    (@start[0] == @dest[0]) || (@start[1] == @dest[1])
+  end
+  def bishop_move
+    (@start[0] - @dest[0]).abs == (@start[1] - @dest[1]).abs
+  end
+
+  def valid_position?
+    start_color = piece_color? @start
+    dest_color = piece_color? @dest
+
+    is_cell_empty? || (start_color != dest_color)
+  end
+
+  def is_cell_empty?
+    @board[@dest[0] - 1][@dest[1] - 1].nil?
+  end
+
+  def piece_color? piece
+    @board[piece[0] - 1][piece[1] - 1].to_s.start_with?("w")
+  end
+end
+
 class Piece
-  def is_valid_position? board, start, dest
-    start_color = piece_color? board, start
-    dest_color = piece_color? board, dest
+  include CheckMove
 
-    is_cell_empty?(board, dest) || (start_color != dest_color)
+  def initialize board, start, dest
+    @board = board
+    @start = start
+    @dest = dest
   end
 
-  private
-
-  def is_cell_empty? board, dest
-    board[dest[0]-1][dest[1]-1].nil?
-  end
-
-  def piece_color? board, pos
-    board[pos[0] - 1][pos[1] - 1].to_s.start_with?("w")
+  def legal_move?
+    if valid_position? && valid_move?
+      puts "LEGAL"
+    else
+      puts "ILEGAL"
+    end
   end
 end
 
 class Rook < Piece
-  def valid_move? board, start, dest
-    is_valid_position?(board, start, dest) && (start[0] == dest[0])|| (start[1] == dest[1])
+  def valid_move?
+    rook_move
   end
 end
 
-rook = Rook.new
-puts rook.valid_move? board, [1,1], [1,7]
+class Bishop < Piece
+  def valid_move?
+    bishop_move
+  end
+end
+
+rook = Rook.new board, [1,1], [1,7]
+rook.legal_move?
+
+bishop = Bishop.new board, [4,2], [5,3]
+bishop.legal_move?
