891feeab776c775201777d1fd4c108b3b3f48e75
rubenmoya <rmoyarodriguez@gmail.com>
2015-10-30 10:12:33 +0100

    Rook movement validations.

--- chess_validator.rb
+++ chess_validator.rb
@@ -17,18 +17,33 @@ board = [
   [nil, nil, nil, nil, nil, nil, nil, nil],
   [nil, nil, nil, nil, nil, nil, nil, nil],
   [nil, nil, nil, nil, nil, nil, nil, nil],
-  [:wR, nil, nil, nil, nil, nil, nil, :wR]
+  [:bR, nil, nil, nil, nil, nil, nil, :wR]
 ]
 
-class Rook
-  def valid_position? board, start, dest
-    board[dest[0]][dest[1]]
-    # board dest empty?
-    # if not, what color is the piece?
-    # cells until dest emtpy?
-    # check if move is horizontal or vertical
+class Piece
+  def is_valid_position? board, start, dest
+    start_color = piece_color? board, start
+    dest_color = piece_color? board, dest
+
+    is_cell_empty?(board, dest) || (start_color != dest_color)
+  end
+
+  private
+
+  def is_cell_empty? board, dest
+    board[dest[0]-1][dest[1]-1].nil?
+  end
+
+  def piece_color? board, pos
+    board[pos[0] - 1][pos[1] - 1].to_s.start_with?("w")
+  end
+end
+
+class Rook < Piece
+  def valid_move? board, start, dest
+    is_valid_position?(board, start, dest) && (start[0] == dest[0])|| (start[1] == dest[1])
   end
 end
 
 rook = Rook.new
-rook.is_valid_position? board, [0,0], [2,2]
+puts rook.valid_move? board, [1,1], [1,7]
